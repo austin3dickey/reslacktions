ea964dd534b901f0ea94f0a64bae93e7ff34e447
Austin Dickey <austin3dickey@gmail.com>
2021-01-26 19:02:38 -0600

    New API and tqdm

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # reslacktions
+
 Scrape Slack emoji reactions and figure out who uses certain ones the most
+
+## Installation
+
+No installation right now. You will need these python packages:
+
+    - pandas
+    - tqdm
+    - slack_sdk
+
+You can install them with `pip install <pkg_name>`.
+
+## Usage
+
+Call the script like `SLACK_TOKEN=xoxp-12345 python reslacktions.py`
--- reslacktions.py
+++ reslacktions.py
@@ -1,52 +1,93 @@
 import os
 import pathlib
+import time
+from typing import Optional
 
 import pandas as pd
+from tqdm import tqdm
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 
 client = WebClient(token=os.environ["SLACK_TOKEN"])
 
 
 def get_users() -> dict:
-    """Get all user IDs and names in this Slack team"""
+    """Get all user IDs and names in this Slack team
+
+    Returns:
+        A dict like {'USERID': 'User Name'}
+    """
     res = client.users_list().validate().data
-    return {user["id"]: user["profile"]["real_name"] for user in res["members"]}
+    return {user["id"]: user["profile"]["real_name"].replace("/", " ") for user in res["members"]}
 
 
 def get_reactions(user_id: str, page_size: int) -> pd.DataFrame:
-    """Get all reactions someone has ever made"""
+    """Get all reactions someone has ever made
+
+    Args:
+        user_id: The user_id to filter to
+        page_size: The max number of items to return
+
+    Returns:
+        A pd.DataFrame with columns 'emoji' (the emoji name) and 'count' (the number of times this
+        user reacted with this emoji)
+    """
     user_reactions = {}
-    current_page = 0
-    all_pages = 1
-    while current_page < all_pages:
-        print(f"Getting page {current_page + 1} of {all_pages}")
-        res = client.reactions_list(
-            user=user_id, count=page_size, page=current_page + 1
-        )
-        res = res.validate().data
-        current_page = res["paging"]["page"]
-        all_pages = res["paging"]["pages"]
-        for item in res["items"]:
-            for react_name in extract_reactions(item, user_id):
-                user_reactions[react_name] = user_reactions.get(react_name, 0) + 1
+    cursor = get_one_page(user_reactions, user_id, page_size, None)
+    while cursor:
+        cursor = get_one_page(user_reactions, user_id, page_size, cursor)
+
     return pd.DataFrame(data=user_reactions.items(), columns=["emoji", "count"])
 
 
-def extract_reactions(item: dict, user_id: str) -> dict:
-    """Slack returns a mixture of messages and file info. Parse through it to get the names of the
-    reactions this user reacted with.
-    """
-    if "message" in item:
-        reacts = item["message"]["reactions"]
-    elif "comment" in item:
-        reacts = item["comment"]["reactions"]
-    elif "file" in item:
-        reacts = item["file"]["reactions"]
-    else:
-        reacts = []
+def get_one_page(
+    user_reactions: dict, user_id: str, page_size: int, cursor: Optional[str]
+) -> Optional[str]:
+    """Modifies the user_reactions dict in place, adding reaction counts based on one page of data
 
-    return [react["name"] for react in reacts if user_id in react["users"]]
+    Args:
+        user_reactions: The dict of {"reaction_name": <count>}
+        user_id: The user_id to filter to
+        page_size: The max number of items to return
+        cursor: Optional cursor in pagination
+
+    Returns:
+        A cursor to the next page, or None or an empty string if it's done
+    """
+    tqdm.write("Getting a page")
+    kwargs = {"user": user_id, "count": page_size}
+    if cursor:
+        kwargs["cursor"] = cursor
+
+    try:
+        res = client.reactions_list(**kwargs).validate().data
+    except SlackApiError as e:
+        if e.response["error"] == "ratelimited":
+            delay = int(e.response.headers["Retry-After"]) + 2
+            tqdm.write(f"Rate limited. Retrying in {delay} seconds")
+            time.sleep(delay)
+            res = client.reactions_list(**kwargs).validate().data
+        elif e.response["error"] == "internal_error":
+            res = {"items": [], "response_metadata": {}}
+        else:
+            raise e
+
+    for item in res["items"]:
+        if "message" in item:
+            reacts = item["message"]["reactions"]
+        elif "comment" in item:
+            reacts = item["comment"]["reactions"]
+        elif "file" in item:
+            reacts = item["file"]["reactions"]
+        else:
+            reacts = []
+
+        for react in reacts:
+            if user_id in react["users"]:
+                user_reactions[react["name"]] = user_reactions.get(react["name"], 0) + 1
+
+    return res["response_metadata"].get("next_cursor", None)
 
 
 if __name__ == "__main__":
@@ -54,16 +95,16 @@ if __name__ == "__main__":
     all_reactions = {}
     react_dir = pathlib.Path("reactions")
     os.makedirs(react_dir, exist_ok=True)
-    for user_id, user_name in users.items():
+    for user_id, user_name in tqdm(users.items()):
         file = react_dir / f"{user_name}.pkl"
         if not file.exists():
-            print(f"Querying {user_name}")
+            tqdm.write(f"Querying {user_name}")
             # cache to file in case something happens
-            get_reactions(user_id, 100).to_pickle(file)
+            get_reactions(user_id, page_size=1000).to_pickle(file)
 
     # read them back in
     files = react_dir.glob("*.pkl")
     react_dict = {file.parts[1][:-4]: pd.read_pickle(file) for file in files}
     all_reacts = pd.concat(react_dict, names=["name"])
     all_reacts = all_reacts.reset_index(-1, drop=True).reset_index()
-    all_reacts.to_csv("all_reactions.csv")
+    all_reacts.to_csv("all_reactions.csv", index=False)
